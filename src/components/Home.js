c32f50cb947ee0b286daa76444cea4d04f0f1f98
pudu87 <sam.cappelle87@gmail.com>
2021-01-25 13:43:43 -0300

    style home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,33 @@
 import meatballs from '../img/meatballs.jpeg'
+import iceCream from '../img/ice-cream.jpeg'
+import daimCake from '../img/daim-cake.jpeg'
 
 function Home() {
   return (
     <section className='home'>
-      <img src={meatballs}/>
-      <p>We also sell furniture.</p>
+      <div className='banner'>
+        <div 
+          className='image-1'
+          style={ { backgroundImage: `url(${daimCake})` } } >
+        </div>
+        <div 
+          className='image-2'
+          style={ { backgroundImage: `url(${meatballs})` } } >
+        </div>
+        <div 
+          className='image-3'
+          style={ { backgroundImage: `url(${iceCream})` } } >
+        </div>
+        <p className='p-1'>
+          Your favourite Scandinavian restaurant...
+        </p>
+        <p className='p-2'>
+          ...now also sells furniture!
+        </p>
+        <p className='p-3'>
+          Check our shop!
+        </p>
+      </div>
     </section>
   );
 }
