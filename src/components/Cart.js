a4cfcc4bdf492bfeae96a340c370157717ebf706
pudu87 <sam.cappelle87@gmail.com>
2021-01-26 15:05:23 -0300

    style cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,21 +1,48 @@
 function Cart(props) {
-  const { quantities, products } = props;
+  const { quantities, items } = props;
 
-  const listItems = quantities.map((quantity, index) => {
-    return (
-      <li key={index}>
-        {products[index].name} : {quantity}
-      </li>
-    )
+  const calculateItemTotal = (quantity, index) => {
+    return quantity * items[index].price;
+  }
+
+  const calculateTotal = () => {
+    return quantities.reduce((sum, quantity, index) => {
+      return sum + (quantity * items[index].price);
+    }, 0).toLocaleString()
+  }
+
+  const itemTable = quantities.map((quantity, index) => {
+    if (quantity !== 0) {
+      return (
+        <tr key={index}>
+          <th key={`${index}-1`}>
+            {items[index].name}
+          </th>
+          <th key={`${index}-2`}>
+            × {quantity}
+          </th>
+          <th key={`${index}-3`}>
+            {calculateItemTotal(quantity, index).toLocaleString()}
+          </th>
+        </tr>
+      )
+    }
   })
 
   return (
     <section 
       className='cart hide'>
-      {listItems}
-      <button>
-        Checkout
-      </button>
+      <table>
+        <tbody>
+          {itemTable}
+          <tr>
+            <th>Total</th>
+            <th></th>
+            <th>{calculateTotal()}</th>
+          </tr>
+        </tbody>
+      </table>
+      <button>Checkout</button>
     </section>
   )
 }
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -10,7 +10,7 @@ import klippan from '../img/klippan.webp';
 import lack from '../img/lack.webp';
 
 function Shop() {
-  const products = [
+  const items = [
     { src: billy, name: 'Billy', type: 'Book Case', price: 40000 },
     { src: ektorp, name: 'Ektorp', type: 'Sofa', price: 340000 },
     { src: fargrik, name: 'Färgrik', type: 'Mug', price: 700 },
@@ -19,14 +19,14 @@ function Shop() {
     { src: lack, name: 'Lack', type: 'Side Table', price: 8000 }
   ];
 
-  const [quantities, setQuantities] = useState(Array(products.length).fill(0));
+  const [quantities, setQuantities] = useState(Array(items.length).fill(0));
 
   const toggleCart = () => {
     document.querySelector('.cart').classList.toggle('hide');
     document.querySelector('.cart-link').classList.toggle('change-background');
   }
 
-  const addProduct = (e) => {
+  const additem = (e) => {
     e.preventDefault();
     const index = Number(e.target.closest('li').className.split('_')[1]);
     const newQuantity = Number(e.target.firstChild.value);
@@ -35,20 +35,24 @@ function Shop() {
     setQuantities([...before, newQuantity, ...after]);
   }
 
-  const listItems = products.map((product, index) => {
+  const itemsInCart = () => {
+    return quantities.reduce((sum, i) => i + sum);
+  }
+
+  const listItems = items.map((item, index) => {
     return (
       <li 
         key={index}
         className={`item_${index}`}>
-        <img src={product.src}/>
-        <div className='product-specs'>
+        <img src={item.src}/>
+        <div className='item-specs'>
           <div className='info'>
-            <h2>{product.name}</h2>
-            <p className='type'>{product.type}</p>
-            <p className='price'>$ {product.price.toLocaleString()}</p>
+            <h2>{item.name}</h2>
+            <p className='type'>{item.type}</p>
+            <p className='price'>$ {item.price.toLocaleString()}</p>
           </div>
           <div className='purchase'>
-            <form onSubmit={addProduct}>
+            <form onSubmit={additem}>
               <input 
                 type='number'
                 className='quantity'
@@ -74,14 +78,14 @@ function Shop() {
         onClick={toggleCart}>
         <FontAwesomeIcon
           icon='shopping-cart' />
-        <span>({quantities.reduce((sum, i) => i + sum)})</span> 
+        <span>({itemsInCart()})</span> 
       </div>
-      <ul className='products'>
+      <ul className='items'>
         {listItems}
       </ul>
       <Cart 
         quantities={quantities}
-        products={products} />
+        items={items} />
     </section>
   );
 }
